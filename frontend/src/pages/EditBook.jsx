e70fbb7ca0e6a12f308b983d5f9c305af8f3d18f
himanshu-chaurasia <135429868+himanshu-431@users.noreply.github.com>
2023-12-06 22:15:41 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -37,7 +37,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`book-store-lovat-ten.vercel.app/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Edited successfully', { variant: 'success' });
